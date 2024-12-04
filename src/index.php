84e64d96207f29569d2ec4d7f848b0da33117064
Mike Little <mike@zed1.com>
2024-12-04 16:14:30 +0000

    more changes to reproduce the rpoblem

--- src/index.php
+++ src/index.php
@@ -1,6 +1,13 @@
 <?php
 
+function add_filter( string $hook, callable $function, int $priority = 10, int $accepted_args = 1 ) {
+	//return add_action( $hook, $function, $priority, $accepted_args );
+	return $hook;
+}
+
 echo 'Hello World!';
 syntax_error;
 $test = 'testing';
 echo $test ?? 'nothing';
+
+add_filter('get_the_guid', fn() => '');
